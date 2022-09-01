cd5b179e25add404697827e8e657ca8b11c5cba6
nanazzoo <>
2022-09-01 15:46:32 +0900

    Update 03_GROUP BY_HAVING.sql

--- 03_GROUP BY_HAVING.sql
+++ 03_GROUP BY_HAVING.sql
@@ -18,8 +18,40 @@
 -- 그룹 함수는 단 한개의 결과 값만 산출하기 때문에 그룹이 여러 개일 경우 오류 발생
 -- 여러 개의 결과 값을 산출하기 위해 그룹 함수가 적용된 그룹의 기준을 ORDER BY절에 기술하여 사용
 
+-- EMPLOYEE 테이블에서 부서코드, 부서별 급여 합 조회
+
+-- 1) 부서 코드만 조회
+SELECT DEPT_CODE FROM EMPLOYEE; -- 23행
+
+-- 2) 전체 급여 합 조회
+SELECT SUM(SALARY) FROM EMPLOYEE; -- 1행
+
+-- 3) 그룹으로 나눠서 조회
+SELECT DEPT_CODE, SUM(SALARY) 
+FROM EMPLOYEE
+GROUP BY DEPT_CODE; --> DEPT_CODE가 같은 행 끼리 하나의 그룹이 됨
+-- ORA-00937: 단일 그룹의 그룹 함수가 아닙니다
 
 
+-- EMPLOYEE 테이블에서
+-- 직급 코드가 같은 사람의 직급코드, 급여 평균, 인원 수를
+-- 직급 코드 오름 차순으로 조회
+
+SELECT JOB_CODE, ROUND(AVG(SALARY)), COUNT(*) 
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+ORDER BY JOB_CODE;
+
+
+-- EMPLOYEE 테이블에서 
+-- 성별(남, 여)과 각 성별 별 인원 수, 급여 합을 
+-- 인원 수 오름 차순으로 조회
+SELECT DECODE(SUBSTR(EMP_NO, 8, 1), '1', '남', '2', '여') 성별, 
+	COUNT(*) "인원 수" , 
+	SUM(SALARY) "급여 합"
+FROM EMPLOYEE
+GROUP BY DECODE(SUBSTR(EMP_NO, 8, 1), '1', '남', '2', '여') -- 별칭 사용X (SELECT절 해석 아직X)
+ORDER BY "인원 수"; -- 별칭 사용 O (SELECT절 해석 완료) 
 
 
 
@@ -28,6 +60,38 @@
 -- * WHERE절 GROUP BY절을 혼합하여 사용
 --> WHERE절은 각 컬럼 값에 대한 조건 (SELECT문 해석 순서를 잘 기억하는 것이 중요!!)
 
+/*
+ SELECT문 해석 순서
+ 
+ 5. SELECT
+ 1. FROM
+ 2. WHERE
+ 3. GROUP BY
+ 4. HAVING
+ 6. ORDER BY
+ 
+ */
+
+
+-- EMPLOYEE 테이블에서 부서코드가 D5, D6인 부서의 평균 급여, 인원 수 조회
+
+SELECT DEPT_CODE, ROUND(AVG(SALARY)), COUNT(*) 
+FROM EMPLOYEE
+WHERE DEPT_CODE IN ('D5', 'D6')
+GROUP BY DEPT_CODE
+ORDER BY 1;
+
+
+-- EMPLOYEE 테이블에서 직급 별 2000년도 이후(2000년도 포함) 입사자들의 급여 합을 조회
+
+SELECT JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+WHERE 
+	EXTRACT(YEAR FROM HIRE_DATE) >= 2000
+--	HIRE_DATE >= TO_DATE('2000-01-01')
+--	TO_CHAR(HIRE_DATE, 'YYYY') >= '2000'
+GROUP BY JOB_CODE
+ORDER BY 1;
 
 
 
@@ -36,9 +100,17 @@
 -- * 여러 컬럼을 묶어서 그룹으로 지정 가능 --> 그룹내 그룹 이 가능하다!
 -- *** GROUP BY 사용시 주의사항
 --> SELECT문에 GROUP BY절을 사용할 경우
---  SELECT절에 명시한 조회할려면 컬럼 중
+--  SELECT절에 명시한 조회하려는 컬럼 중
 --  그룹함수가 적용되지 않은 컬럼을 
 --  모두 GROUP BY절에 작성해야함.
+
+
+-- EMPLOYEE 테이블에서 부서별로 같은 직급인 사원의 수를 조회
+-- (부서코드 오름차순, 직급코드 내림차순)
+SELECT DEPT_CODE, JOB_CODE, COUNT(*)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE, JOB_CODE -- DEPT_CODE로 그룹을 나누고, 나눠진 그룹 내에서 JOB_CODE로 또 그룹을 분류
+ORDER BY 1, 2 DESC;
 
 
 
@@ -48,3 +120,45 @@
 
 -- * HAVING 절 : 그룹함수로 구해 올 그룹에 대한 조건을 설정할 때 사용
 -- HAVING 컬럼명 | 함수식 비교연산자 비교값
+
+
+
+/*
+ SELECT문 해석 순서
+ 
+ 5. SELECT
+ 1. FROM
+ 2. WHERE
+ 3. GROUP BY
+ 4. HAVING
+ 6. ORDER BY
+ 
+ */
+
+-- 부서별 평균 급여가 300만원 이상인 부서를 조회(부서코드 오름차순)
+SELECT DEPT_CODE 부서, ROUND(AVG(SALARY)) "평균 급여"
+FROM EMPLOYEE e 
+-- WHERE SALARY >= 3000000 --> 한 사람의 급여가 300백만 이상이라는 조건(요구사항 불일치)
+GROUP BY DEPT_CODE
+HAVING ROUND(AVG(SALARY)) >= 3000000 --> DEPT_CODE 그룹 중 평균 급여가 300만원 이상인 그룹만 조회
+ORDER BY 1;
+
+
+-- EMPLOYEE 테이블에서 직급별 인원수가 5명 이하인 직급코드, 인원수 조회(직급 코드 오름차순)
+SELECT JOB_CODE, COUNT(*)
+FROM EMPLOYEE e 
+GROUP BY JOB_CODE
+HAVING COUNT(*) <= 5 -- HAVING 절에는 그룹 함수가 반드시 작성된다!
+ORDER BY 1;
+
+
+
+
+
+
+
+
+
+
+
+
